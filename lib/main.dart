861a59480c8c79942b0aa4e112be1b795566b4a5
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-05-03 16:32:52 -0300

    Adding/Removing wordPair to the Set<WordPair> when heart icon is clicked

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,15 @@ final TextStyle _biggerFont = TextStyle(fontSize: 18.0);
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
